88f7c74084a5c0b24f2806e26686b2bdda7ec494
Ryan <dyrshavon@gmail.com>
2018-01-25 20:02:32 -0500

    Second commit, starting on the test program

---
+++ Readings/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Readings.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Novel> Novels { get; set; }
+    }
+}
---
+++ Readings/Models/Novel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web;
+
+namespace Readings.Models
+{
+    public class Novel
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+
+
+
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
