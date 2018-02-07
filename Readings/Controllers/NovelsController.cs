07fc906581001c10ea407f3092ed9c96f601bc8c
Ryan <dyrshavon@gmail.com>
2018-02-07 16:19:30 -0500

    backend stuff done

---
+++ Readings/Controllers/NovelsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Readings.Models;
+
+namespace Readings.Controllers
+{
+    public class NovelsController : Controller
+    {
+        private ReadingsContext db = new ReadingsContext();
+
+        // GET: Novels
+        public ActionResult Index()
+        {
+            var novels = db.Novels.Include(n => n.Category);
+            return View(novels.ToList());
+        }
+
+        // GET: Novels/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Novel novel = db.Novels.Find(id);
+            if (novel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(novel);
+        }
+
+        // GET: Novels/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(db.Categories, "Id", "Name");
+            return View();
+        }
+
+        // POST: Novels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,Description,Author,CategoryID")] Novel novel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Novels.Add(novel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(db.Categories, "Id", "Name", novel.CategoryID);
+            return View(novel);
+        }
+
+        // GET: Novels/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Novel novel = db.Novels.Find(id);
+            if (novel == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "Id", "Name", novel.CategoryID);
+            return View(novel);
+        }
+
+        // POST: Novels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,Description,Author,CategoryID")] Novel novel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(novel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "Id", "Name", novel.CategoryID);
+            return View(novel);
+        }
+
+        // GET: Novels/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Novel novel = db.Novels.Find(id);
+            if (novel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(novel);
+        }
+
+        // POST: Novels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Novel novel = db.Novels.Find(id);
+            db.Novels.Remove(novel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Readings/Models/ReadingsContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Readings.Models
+{
+    public class ReadingsContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public ReadingsContext() : base("name=ReadingsContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Readings.Models.Novel> Novels { get; set; }
+
+        public System.Data.Entity.DbSet<Readings.Models.Category> Categories { get; set; }
+    }
+}
